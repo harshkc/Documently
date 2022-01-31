88ff54cd58351310bed3e7f76aea9222dfc8b838
harshkc <harsh.hkc1006@gmail.com>
2022-01-31 23:21:10 +0530

    added CRD methods on transactions

--- src/context/transactions.js
+++ src/context/transactions.js
@@ -1,72 +1,95 @@
-import React, {
-  useReducer,
-  createContext,
-  useContext,
-  useCallback,
-} from "react";
+import React, {useReducer, createContext, useContext, useCallback} from "react";
+import {db} from "../firebase";
+import {collection, doc, getDocs, setDoc, deleteDoc} from "firebase/firestore";
+import {useEffect} from "react";
 
 const TransactionContext = createContext();
 TransactionContext.displayName = "TransactionContext";
 
-const initialTransaction =
-  JSON.parse(localStorage.getItem("transactions")) || getDummyTransactions();
+const initialTransaction = [];
 
 const transactionsReducer = (transactions, action) => {
-  const { type, payload } = action;
+  const {type, payload} = action;
   let newTransactions;
 
   switch (type) {
     case "ADD_TRANSACTION":
-      newTransactions = [payload, ...transactions];
+      newTransactions = [...transactions, payload];
       localStorage.setItem("transactions", JSON.stringify(newTransactions));
       return newTransactions;
     case "DELETE_TRANSACTION":
-      newTransactions = transactions.filter(
-        (transaction) => transaction.id !== payload
-      );
+      newTransactions = transactions?.filter((transaction) => transaction.id !== payload);
       localStorage.setItem("transactions", JSON.stringify(newTransactions));
       return newTransactions;
+    case "UPDATE_TRANSACTIONS":
+      localStorage.setItem("transactions", JSON.stringify(payload));
+      return payload;
     default:
       throw new Error("Undefined action passed in transactionsReducer");
   }
 };
 
-const TransactionProvider = ({ children }) => {
-  const [transactions, dispatch] = useReducer(
-    transactionsReducer,
-    initialTransaction
-  );
+const TransactionProvider = ({children, user}) => {
+  const [transactions, dispatch] = useReducer(transactionsReducer, initialTransaction);
 
-  const addTransaction = useCallback(
-    (transaction) => {
-      dispatch({
-        type: "ADD_TRANSACTION",
-        payload: transaction,
-      });
-    },
-    [dispatch]
-  );
+  const getTransactions = async () => {
+    try {
+      const transactionsSnapshot = await getDocs(collection(db, "users", user.id, "transactions"));
+      const transactions = transactionsSnapshot.docs.map((doc) => doc.data());
+      dispatch({type: "UPDATE_TRANSACTIONS", payload: transactions});
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  const deleteTransaction = useCallback(
-    (id) => {
-      dispatch({
-        type: "DELETE_TRANSACTION",
-        payload: id,
-      });
-    },
-    [dispatch]
-  );
+  const addTransactionToDB = async (transaction) => {
+    try {
+      await setDoc(doc(db, "users", user.id, "transactions", transaction.id), transaction);
+      console.log("Transaction added to firestore");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const balance = transactions.reduce((acc, currVal) => {
-    return currVal.type === "Expense"
-      ? acc - currVal.amount
-      : acc + currVal.amount;
-  }, 0);
+  const deleteTransactionFromDB = async (transactionID) => {
+    try {
+      const toDeleteRef = doc(db, "users", user.id, "transactions", transactionID);
+      await deleteDoc(toDeleteRef);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  const addTransaction = async (transaction) => {
+    await addTransactionToDB(transaction);
+
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  };
+
+  const deleteTransaction = async (id) => {
+    await deleteTransactionFromDB(id);
+
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
+  const balance = transactions.length
+    ? transactions?.reduce((acc, currVal) => {
+        return currVal.type === "Expense" ? acc - currVal.amount : acc + currVal.amount;
+      }, 0)
+    : 0;
 
   return (
-    <TransactionContext.Provider
-      value={{ transactions, addTransaction, deleteTransaction, balance }}
-    >
+    <TransactionContext.Provider value={{transactions, addTransaction, deleteTransaction, balance}}>
       {children}
     </TransactionContext.Provider>
   );
@@ -75,51 +98,9 @@ const TransactionProvider = ({ children }) => {
 const useTransactionContext = () => {
   const context = useContext(TransactionContext);
   if (!context) {
-    throw new Error(
-      "useTransactionContext must be used within a TransactionProvider"
-    );
+    throw new Error("useTransactionContext must be used within a TransactionProvider");
   }
   return context;
 };
 
-function getDummyTransactions() {
-  return [
-    {
-      amount: 500,
-      category: "Salary",
-      type: "Income",
-      date: "2020-11-16",
-      id: "44c68123-5b86-4cc8-b915-bb9e16cebe6a",
-    },
-    {
-      amount: 225,
-      category: "Investments",
-      type: "Income",
-      date: "2020-11-16",
-      id: "33b295b8-a8cb-49f0-8f0d-bb260686de1a",
-    },
-    {
-      amount: 123,
-      category: "Car",
-      type: "Expense",
-      date: "2020-11-16",
-      id: "0f72e66e-e144-4a72-bbc1-c3c90018635e",
-    },
-    {
-      amount: 50,
-      category: "Pets",
-      type: "Expense",
-      date: "2020-11-13",
-      id: "c5647dde-d857-463d-8b4e-1c966cc5f83e",
-    },
-    {
-      amount: 500,
-      category: "Travel",
-      type: "Expense",
-      date: "2020-11-13",
-      id: "365a4ebd-9892-4471-ad55-36077e0121a9",
-    },
-  ];
-}
-
-export { TransactionProvider, useTransactionContext };
+export {TransactionProvider, useTransactionContext};
