d2d27e194ee6e5d167bbf9de977ab36bca6da48a
harshkc <harsh.hkc1006@gmail.com>
2022-02-01 23:07:50 +0530

    made journaly UI

--- src/components/journal/Notes.js
+++ src/components/journal/Notes.js
@@ -2,37 +2,52 @@ import React from "react";
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from "@material-ui/core";
 import useStyles from "./styles";
 
-const Notes = ({children}) => {
+const Notes = ({user}) => {
   const {root, cardContent, divider} = useStyles();
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
     <Card className={root}>
       <Grid container spacing={2}>
         <Grid item xs={10}>
-          <CardHeader title={<strong>Xpensly</strong>} style={{color: "#E77C97"}} />
-        </Grid>
-        <Grid item xs={2}>
-          {children}
+          <CardHeader
+            title={
+              <Typography variant='h3' style={{fontWeight: "bold"}}>
+                Journaly
+              </Typography>
+            }
+            style={{color: "#E77C97"}}
+          />
         </Grid>
       </Grid>
       <CardContent className={cardContent}>
-        <Typography variant='h6'>
-          Total Balance:
-          <span style={{color: "green"}}>
-            <strong> ₹balance</strong>
-          </span>
-        </Typography>
-        <Typography variant='subtitle2' style={{lineHeight: "1.5em", marginTop: "20px"}}>
-          Hello
-        </Typography>
+        <Typography variant='h6'>{new Date().toLocaleDateString()}</Typography>
         <Divider className={divider} />
       </CardContent>
       <CardContent className={cardContent}>
-        <Grid container spacing={2}>
+        <Grid container>
           <Grid item xs={12}>
             <Typography variant='h6'>
-              <strong>Notes</strong>
+              <strong>Reflect Your Day</strong>
             </Typography>
+            <textarea
+              style={{
+                margin: "10px 0",
+                padding: "8px 12px",
+                border: "1px solid #f1f1f4",
+                background: "#f1f1f4",
+                borderRadius: "5px",
+                width: "100%",
+                maxWidth: "100%",
+                maxHeight: "50vh",
+                height: "50vh",
+              }}
+              placeholder='Write down what you observed today, key things you learned or great experiences'
+              onChange={handleChange}
+            />
           </Grid>
         </Grid>
       </CardContent>
--- src/screens/Journaly.js
+++ src/screens/Journaly.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Notes from "../components/journal/Notes";
+import useStyles from "./journalStyles";
+import {Grid} from "@material-ui/core";
 
 const Journal = ({user}) => {
+  const {grid} = useStyles();
   return (
-    <div
-      style={{
-        maxWidth: "70vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Grid
+      className={grid}
+      container
+      spacing={0}
+      alignItems='center'
+      justifyContent='center'
+      style={{height: "100vh"}}
     >
-      <Notes />
-    </div>
+      <Grid item xs={12} sm={9} md={8} lg={8}>
+        <Notes user={user} />
+      </Grid>
+    </Grid>
   );
 };
 
