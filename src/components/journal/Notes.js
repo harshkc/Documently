171b5cf02c7d15a76001d8ca5f0f80dba96bba86
Harsh Choudhary <harsh.hkc1006@gmail.com>
2022-02-02 16:08:45 +0530

    :tada:feat: adds read and write journals feature (#26)
    * CRUD journals
    
    * added save icon
    
    * :tada: feat:added fetch journals method

--- src/components/journal/Notes.js
+++ src/components/journal/Notes.js
@@ -1,18 +1,73 @@
 import React from "react";
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from "@material-ui/core";
 import useStyles from "./styles";
+import {db} from "../../firebase";
+import {doc, getDoc, setDoc} from "firebase/firestore";
+import {toShortFormat} from "../../utils/formatDate";
+import {CircularProgress, Fab} from "@material-ui/core";
+import CheckIcon from "@material-ui/icons/Check";
+import SaveIcon from "@material-ui/icons/Save";
+import {MuiPickersUtilsProvider, KeyboardDatePicker} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 const Notes = ({user}) => {
-  const {root, cardContent, divider} = useStyles();
+  const [note, setNote] = React.useState("");
+  const [isSaving, setSaving] = React.useState(false);
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [theDay, setTheDay] = React.useState(toShortFormat(new Date()));
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
+  const timeout = React.useRef();
+
+  const getNotes = async (date) => {
+    try {
+      const noteSnapshot = await getDoc(doc(db, "users", user.id, "notes", date));
+      if (noteSnapshot.exists()) {
+        setNote(noteSnapshot.data());
+      } else {
+        console.log("No notes found");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
+  React.useEffect(() => {
+    getNotes(theDay);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const addNoteToDB = async (value, theDay) => {
+    try {
+      console.log("note", note);
+      await setDoc(doc(db, "users", user.id, "notes", theDay), {content: value});
+      console.log("Note added to firestore");
+    } catch (error) {
+      console.log(error);
+    }
+    setSaving(false);
+  };
+
+  const handleChange = (e) => {
+    clearTimeout(timeout.current);
+    setSaving(true);
+    setNote(e.target.value);
+    timeout.current = setTimeout(() => {
+      addNoteToDB(e.target.value, theDay);
+    }, 3000);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    const theDay = toShortFormat(date);
+    getNotes(theDay);
+    setTheDay(theDay);
+  };
+
+  const {root, cardContent, divider, wrapper, fabProgress, flexed} = useStyles();
   return (
     <Card className={root}>
       <Grid container spacing={2}>
-        <Grid item xs={10}>
+        <Grid item xs={8}>
           <CardHeader
             title={
               <Typography variant='h3' style={{fontWeight: "bold"}}>
@@ -22,17 +77,44 @@ const Notes = ({user}) => {
             style={{color: "#E77C97"}}
           />
         </Grid>
+        <Grid item xs={4}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant='inline'
+              format='dd/MM/yyyy'
+              margin='normal'
+              id='date-picker-inline'
+              label='Fetch Journals'
+              minDate={new Date("2022-02-01")}
+              maxDate={new Date()}
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
       </Grid>
       <CardContent className={cardContent}>
-        <Typography variant='h6'>{new Date().toLocaleDateString()}</Typography>
+        <Typography variant='h6'>{theDay}</Typography>
         <Divider className={divider} />
       </CardContent>
       <CardContent className={cardContent}>
         <Grid container>
           <Grid item xs={12}>
-            <Typography variant='h6'>
-              <strong>Reflect Your Day</strong>
-            </Typography>
+            <div className={flexed}>
+              <span style={{fontSize: "22px", fontWeight: "bold", marginRight: "0.5rem"}}>
+                Reflect Your Day
+              </span>
+              <div className={wrapper}>
+                <Fab size='small' aria-label='save' color='inherit'>
+                  {!isSaving ? <CheckIcon /> : <SaveIcon />}
+                </Fab>
+                {isSaving && <CircularProgress size={51} className={fabProgress} />}
+              </div>
+            </div>
             <textarea
               style={{
                 margin: "10px 0",
@@ -45,8 +127,9 @@ const Notes = ({user}) => {
                 maxHeight: "50vh",
                 height: "50vh",
               }}
-              placeholder='Write down what you observed today, key things you learned or great experiences'
+              placeholder='Write down what you observed today? Key things you learned OR Great Experiences You had'
               onChange={handleChange}
+              value={note.content}
             />
           </Grid>
         </Grid>
--- src/components/journal/styles.js
+++ src/components/journal/styles.js
@@ -1,14 +1,36 @@
 import {makeStyles} from "@material-ui/core/styles";
+import {blueGrey} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    opacity: "0.98",
     boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
     borderRadius: "10px",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+  buttonSuccess: {
+    backgroundColor: blueGrey[500],
+    "&:hover": {
+      backgroundColor: blueGrey[700],
+    },
+  },
+  fabProgress: {
+    color: blueGrey[500],
+    position: "absolute",
+    top: -6,
+    left: -6,
+    zIndex: 1,
   },
   media: {
     height: 0,
     paddingTop: "56.25%", // 16:9
+  },
+  flexed: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
   expand: {
     transform: "rotate(0deg)",
